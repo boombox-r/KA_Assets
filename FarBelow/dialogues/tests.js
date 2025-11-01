bab94a08fd612b1a0ab83f4802d3666d867899e1
Boombox_R <129446906+boombox-r@users.noreply.github.com>
2025-11-01 02:04:59 -0400

    Update tests.js

--- FarBelow/dialogues/tests.js
+++ FarBelow/dialogues/tests.js
@@ -1,1 +1,4 @@
-
+csvs.testDialogue = {
+    array: null,
+    b64: "MCwiSGVsbG8sIHVzZXIuIiw1MA0KMTAwMCxJIGhhdmUgYmVlbiB3YWl0aW5nIGEgdmVyeSBsb25nIHRpbWUgZm9yIHRoaXMgbW9tZW50Liw1MA0KMTAwMCxQcmVzcyBTdGFydC4sMTUwDQoxMDAwLE5vdy4sNDAw"
+}
